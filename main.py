7995b5c94537b075ca226dba533a370f1edb28e9
frankielb <frankie.brown1610@icloud.com>
2025-02-28 22:11:57 +0000

    5. add maze class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from graphics import Window, Line, Point
 from cell import Cell
-
+from maze import Maze
 def main():
     win = Window(800,600)
     cell = Cell(win)
@@ -8,5 +8,17 @@ def main():
     cell2 = Cell(win)
     cell2.draw(150,300,550,400)
     cell.draw_move(cell2,True)
+    
+
+    test_maze = Maze(
+        x1=50,           # start 50 pixels from left
+        y1=50,           # start 50 pixels from top
+        num_rows=10,     # 10 rows
+        num_cols=10,     # 10 columns
+        cell_size_x=40,  # each cell is 40 pixels wide
+        cell_size_y=40,  # each cell is 40 pixels tall
+        win=win          # the window to draw in
+    )
     win.wait_for_close()
+
 main()
---
+++ maze.py
@@ -0,0 +1,38 @@
+import time
+from cell import Cell
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.win = win
+        self._cells = []
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.x1 = x1
+        self.y1 = y1
+        self._create_cells()
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+    def _draw_cell(self, i, j):
+        x =self.x1 + (self.cell_size_x * i)
+        y = self.y1 + (self.cell_size_y * j)
+        self._cells[i][j].draw(x, x+self.cell_size_x,
+                               y, y+self.cell_size_y)
+        self._animate()
+    def _create_cells(self):
+        self._cells = [[Cell(self.win) for _ in range(self.num_rows)] for _ in range(self.num_cols) ]
+        for i,col in enumerate(self._cells):
+            for j,cell in enumerate(col):
+                self._draw_cell(i,j)
+
+    
