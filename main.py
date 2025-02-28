3691bc677f94f1df5819e0ed2216e274d054c8bd
frankielb <frankie.brown1610@icloud.com>
2025-02-28 20:19:17 +0000

    3. add cell

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800,600)
+    cell = Cell(10,100,500,200,win)
+    cell.draw()
 
-    win.draw_line(Line(Point(10,150), Point(500,10)), 'black')
     win.wait_for_close()
 main()
