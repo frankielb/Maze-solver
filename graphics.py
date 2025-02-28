12371a2f85b365b49b3ec460fb5d3b833146f4cf
frankielb <frankie.brown1610@icloud.com>
2025-02-28 20:28:29 +0000

    3.2 move stuff around

--- graphics.py
+++ graphics.py
@@ -40,28 +40,5 @@ class Line:
             self.point2.x, self.point2.y,
             fill = colour, width =2)
         
-class Cell:
-    def __init__(self, x1, x2, y1, y2, window):
-        self.has_left_wall = True
-        self.has_right_wall = True
-        self.has_top_wall = True
-        self.has_bottom_wall = True
-        self._x1 = x1
-        self._x2 = x2
-        self._y1 = y1
-        self._y2 = y2
-        self._win = window
-    def draw(self):
-        top_left = Point(self._x1, self._y1)
-        top_right = Point(self._x2, self._y1)
-        bottom_left = Point(self._x1, self._y2)
-        bottom_right = Point(self._x2, self._y2)
-        if self.has_left_wall:
-            self._win.draw_line(Line(top_left,bottom_left))
-        if self.has_right_wall:
-            self._win.draw_line(Line(top_right,bottom_right))
-        if self.has_top_wall:
-            self._win.draw_line(Line(top_left,top_right))
-        if self.has_bottom_wall:
-            self._win.draw_line(Line(bottom_left,bottom_right))
+
         
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from graphics import Window, Line, Point, Cell
+from graphics import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800,600)
-    cell = Cell(10,100,500,200,win)
-    cell.draw()
+    cell = Cell(win)
+    cell.draw(10,100,500,200)
 
     win.wait_for_close()
 main()
