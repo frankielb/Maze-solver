ffe59c501016c82b4dc3a6b0e16ab3cc1d48d95a
frankielb <frankie.brown1610@icloud.com>
2025-03-01 12:08:58 +0000

    5.1 add tests

--- maze.py
+++ maze.py
@@ -9,7 +9,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win = None,
     ):
         self.win = win
         self._cells = []
@@ -20,15 +20,22 @@ class Maze:
         self.x1 = x1
         self.y1 = y1
         self._create_cells()
+
     def _animate(self):
+        if self.win is None:
+            return
         self.win.redraw()
         time.sleep(0.05)
+
     def _draw_cell(self, i, j):
+        if self.win is None:
+            return
         x =self.x1 + (self.cell_size_x * i)
         y = self.y1 + (self.cell_size_y * j)
         self._cells[i][j].draw(x, x+self.cell_size_x,
                                y, y+self.cell_size_y)
         self._animate()
+
     def _create_cells(self):
         self._cells = [[Cell(self.win) for _ in range(self.num_rows)] for _ in range(self.num_cols) ]
         for i,col in enumerate(self._cells):
